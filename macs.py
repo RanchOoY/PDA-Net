567f0479dedb227920ac8f24b5bb36a910cda39f
RanchOoY <149474063+RanchOoY@users.noreply.github.com>
2026-09-02 13:17:41 +0800

    Update macs.py

--- macs.py
+++ macs.py
@@ -1,10 +1,10 @@
 import torch
 from torchprofile import profile_macs
-from model_best import LYT
+from model_best import PDA
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = LYT().to(device)
+model = PDA().to(device)
 
 input_tensor = torch.randn(1, 3, 256, 256).to(device) 
 
